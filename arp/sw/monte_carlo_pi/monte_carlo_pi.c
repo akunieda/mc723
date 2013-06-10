44f2ac4e35209d749c16e56ba606fcced77be08c
Alexandre Nobuo Kunieda <alexandre.kunieda@gmail.com>
2013-06-10 09:23:09 -0300

    Global counter in Monte Carlo Pi

--- arp/sw/monte_carlo_pi/monte_carlo_pi.c
+++ arp/sw/monte_carlo_pi/monte_carlo_pi.c
@@ -4,22 +4,26 @@
 
 const int NITER = 1024;
 const float R = 0.5f;
+volatile int hit = 0;
+volatile int total = 0;
 
 int main(int argc, char *argv[]){
   int i;
   float x, y;
-  int hit = 0;
+  int local_hit = 0;
 
   volatile float* random = (volatile float*) 0xFFFFFFFE;
   for(i=0;i<NITER;i++) {
     x = *random - R;
     y = *random - R;
-    if (x*x + y*y <= R*R) hit++;
+    if (x*x + y*y <= R*R) local_hit++;
   }
 
   volatile int* lock = (volatile int*) 0xFFFFFFFF;
   while (*lock);
-  printf("4 * %d/%d = %lf\n", hit, NITER, ((double) 4*hit) / ((double) NITER));
+  hit += local_hit;
+  total += NITER;
+  printf("4 * %d/%d = %lf\n", hit, total, ((double) 4*hit) / ((double) total));
   *lock = 0;
 
   exit(0); // To avoid cross-compiler exit routine
